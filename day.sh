ce5a82b59467eb88e80d5a7125047a031937c9f8
CharzQ <cowan.aedin05@gmail.com>
2026-04-04 19:33:11 -0400

    POSIX shells instead of only bash

--- day.sh
+++ day.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Daily banking system simulation
 # Runs several frontend sessions, merges transactions, then runs backend
@@ -16,7 +16,7 @@ OUTPUT_FILE="transaction_output.txt"
 # List of session files
 DAY_FOLDER=$1
 
-SESSIONS=("$DAY_FOLDER/session1.txt" "$DAY_FOLDER/session2.txt" "$DAY_FOLDER/session3.txt")
+SESSIONS="$DAY_FOLDER/session1.txt $DAY_FOLDER/session2.txt $DAY_FOLDER/session3.txt"
 
 # Clean old merged file
 > $MERGED_FILE
@@ -25,18 +25,18 @@ SESSIONS=("$DAY_FOLDER/session1.txt" "$DAY_FOLDER/session2.txt" "$DAY_FOLDER/ses
 echo "Starting Daily Banking Simulation"
 
 # Run Front End for each session
-for SESSION in "${SESSIONS[@]}"
+for SESSION in $SESSIONS
 do
     echo "Processing $SESSION..."
 
     python $FRONTEND $CURRENT_ACCOUNTS $OUTPUT_FILE < $SESSION
 
     # Append to merged file and remove '00' lines
-    grep -v '^00' "$OUTPUT_FILE" >> "$MERGED_FILE"
+    grep -v '^00' $OUTPUT_FILE >> $MERGED_FILE
 done
 
 # Add terminator line
-echo "00                      00000 00000.00  " >> "$MERGED_FILE"
+echo "00                      00000 00000.00  " >> $MERGED_FILE
 
 # Run Back end
 echo "Running Back End..."
